bf87ea45f98dc352ae816b835b124885db042e22
chiro <chironjit2@gmail.com>
2025-08-24 14:37:03 +0600

    CRM

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import image from '../assets/image.png'
 export const Hero = () => {
     return (
         <>
@@ -8,17 +9,26 @@ export const Hero = () => {
                         Take Control of Your CRM Strategy Today!
                     </h5>
 
-                    <p className="text-sm md:text-base mx-auto max-w-2xl text-center mt-6 max-md:px-2">Is Your CRM Holding You Back? Discover What’s Working and What’s Not with <br/> Our Free CRM Self-Assessment Tool!</p>
+                   <div className={'flex flex-col mx-5 md:text-center md:justify-center items-center md:w-[600px] mt-[30px] md:mx-auto'}>
+                       <p >Is Your CRM
+                           Holding You Back? Discover What’s Working and What’s Not with Our Free CRM Self-Assessment
+                           Tool!</p>
+                   </div>
 
-                    <div className="mx-auto w-full flex items-center justify-center gap-3 mt-4">
+                    <div className="mx-auto w-full flex flex-col items-center justify-center gap-3 mt-4">
                         <button
                             className="bg-[#D93E36] hover:bg-black text-white px-6 py-3 rounded-[8px] font-medium transition">
                             Start Your CRM Self-Assessment Now!
                         </button>
-                      
+                        <div className={'mt-[60px]'}>
+                            <img src={image} alt=""/>
+                        </div>
+
                     </div>
+
+
                 </section>
-                
+
             </div>
         </>
     )
